380203bc83790616d26d32c1e7796375910eb3c9
OthmanCharai <otmanchar3i0688@gmail.com>
2022-04-04 18:11:24 +0100

    employe

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,31 @@ Route::prefix('influencers')->name('influencers.')->group(function(){
         }
     );
 });
+
+/**
+ *  entreprise route
+ */
+Route::prefix('entreprises')->name('entreprises.')->group(function(){
+    Route::get('dashbord',function(){
+        return view('entreprise.dashboard');
+    })->name('dashbord');
+    Route::prefix('profile')->name('profile.')->group(function(){
+        Route::get('account',function(){
+            return view('entreprise.profile.account');
+        })->name('account');
+        Route::get('security',function(){
+            return view('entreprise.profile.security');
+        })->name('security');
+        Route::get('products',function(){
+            return view('entreprise.profile.products');
+        })->name('products');
+
+        Route::get('applied_products',function(){
+            return view('entreprise.profile.applied_products');
+        })->name('applied_products');
+
+
+
+        }
+    );
+});
